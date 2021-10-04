c30f58a6893fe428e4869f3964cf4909055606c9
iman-seraji <iman@softrend.com>
2021-10-04 12:16:58 -0700

    Add project files.

---
+++ Test-App/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Test_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWatcher = new System.Windows.Forms.Button();
+            this.lblNotification = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.lblSelectedFiles = new System.Windows.Forms.Label();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnWatcher
+            // 
+            this.btnWatcher.Location = new System.Drawing.Point(73, 99);
+            this.btnWatcher.Name = "btnWatcher";
+            this.btnWatcher.Size = new System.Drawing.Size(110, 23);
+            this.btnWatcher.TabIndex = 0;
+            this.btnWatcher.Text = "Start Watcher";
+            this.btnWatcher.UseVisualStyleBackColor = true;
+            this.btnWatcher.Click += new System.EventHandler(this.btnWatcher_Click);
+            // 
+            // lblNotification
+            // 
+            this.lblNotification.AutoSize = true;
+            this.lblNotification.ForeColor = System.Drawing.Color.Red;
+            this.lblNotification.Location = new System.Drawing.Point(346, 188);
+            this.lblNotification.Name = "lblNotification";
+            this.lblNotification.Size = new System.Drawing.Size(0, 13);
+            this.lblNotification.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select Files";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(99, 33);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(35, 23);
+            this.btnSelectFile.TabIndex = 4;
+            this.btnSelectFile.Text = "...";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // lblSelectedFiles
+            // 
+            this.lblSelectedFiles.AutoSize = true;
+            this.lblSelectedFiles.Location = new System.Drawing.Point(32, 63);
+            this.lblSelectedFiles.Name = "lblSelectedFiles";
+            this.lblSelectedFiles.Size = new System.Drawing.Size(0, 13);
+            this.lblSelectedFiles.TabIndex = 6;
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(147, 38);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(0, 13);
+            this.lblFilePath.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.lblSelectedFiles);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNotification);
+            this.Controls.Add(this.btnWatcher);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWatcher;
+        private System.Windows.Forms.Label lblNotification;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Label lblSelectedFiles;
+        private System.Windows.Forms.Label lblFilePath;
+    }
+}
+
---
+++ Test-App/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        string fileNames, filePath;
+
+        private void Changed(object sender, FileSystemEventArgs e)
+        {
+            if (fileNames.Contains(e.Name))
+            {
+                string NotificationText = e.FullPath.ToString() + " is changed!";
+                ShowNotification(NotificationText);
+            }
+          
+
+
+
+        }
+
+
+
+        private void Deleted(object sender, FileSystemEventArgs e)
+
+        {
+            if (fileNames.Contains(e.Name))
+            {
+                string NotificationText = e.FullPath.ToString() + " is Deleted!";
+                ShowNotification(NotificationText);
+            }
+          
+
+        }
+
+
+        private void Renamed(object sender, FileSystemEventArgs e)
+
+        {
+
+            if (fileNames.Contains(e.Name))
+            {
+                string NotificationText = e.FullPath.ToString() + " is Renamed!";
+                ShowNotification(NotificationText);
+            }
+
+
+
+        }
+
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+
+        {
+            if (fileNames.Contains(e.Name))
+            {
+                string NotificationText = e.FullPath.ToString() + " is Add!";
+                ShowNotification(NotificationText);
+            }
+        }
+
+
+        delegate void SetTextCallback(string text);
+
+        private void ShowNotification(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.lblNotification.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(ShowNotification);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.lblNotification.Text = text;
+            }
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog x = new OpenFileDialog();
+            x.Multiselect = true;
+            x.ShowDialog();
+            string[] arrayFileNames = x.SafeFileNames;
+            foreach (string fileName in arrayFileNames)
+                fileNames += fileName + " - ";
+
+            filePath = x.FileName.Replace(x.SafeFileName, "");
+
+            lblFilePath.Text = filePath;
+
+            lblSelectedFiles.Text = fileNames;
+        }
+
+        private void btnWatcher_Click(object sender, EventArgs e)
+        {
+
+            FileSystemWatcher fwatcher = new FileSystemWatcher();
+
+            fwatcher.Path = filePath;
+
+            fwatcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName;
+
+            fwatcher.Changed += new FileSystemEventHandler(Changed);
+            fwatcher.Created += new FileSystemEventHandler(FileCreated);
+            fwatcher.Deleted += new FileSystemEventHandler(Deleted);
+            fwatcher.Renamed += new RenamedEventHandler(Renamed);
+
+            fwatcher.Filter = "*.txt";
+
+
+            fwatcher.EnableRaisingEvents = true;
+        }
+
+      
+
+
+    }
+}
